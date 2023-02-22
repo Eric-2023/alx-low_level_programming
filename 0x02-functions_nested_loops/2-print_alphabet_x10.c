fab9875d207a4b2ab610f31618985be31c02ffe2
Eric-2023 <ericmusembi2012@gmail.com>
2023-02-22 11:56:11 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,19 @@
 #include "main.h"
 /**
- * main entry - Entry point
- *@c: The character to print
- *
+ * print_alphabet_x10 - prints 10times the alphabet in lowercase
  * Return: void
- *
  */
 void print_alphabet_x10(void)
 {       
         char c;
         int i = 0;
-
-	while (i <= 9)
+	while (i<=9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar (c);
-		}
-		_putchar ('\n');
-			i++;
+	_putchar (c);
+	}
+	_putchar ('\n');
+	i++;
 	}
 }
